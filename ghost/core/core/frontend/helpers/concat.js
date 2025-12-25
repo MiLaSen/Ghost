9ff15e573085a2dcf66caa67254849ab24ed16cd
MilosL <107317240+MiLaSen@users.noreply.github.com>
2025-12-25 07:05:32 +0100

    added simpleString option

--- ghost/core/core/frontend/helpers/concat.js
+++ ghost/core/core/frontend/helpers/concat.js
@@ -3,9 +3,14 @@ const {SafeString} = require('../services/handlebars');
 module.exports = function concat(...args) {
     const options = args.pop();
     const separator = options.hash.separator || '';
-
+    const simpleString = options.hash.simpleString || false;
     // Flatten arrays - if an argument is an array, spread its elements
     const flattenedArgs = args.flat();
-
+    
+    if (simpleString === true) {
+        return (flattenedArgs.join(separator));
+    }
+    else {
     return new SafeString(flattenedArgs.join(separator));
+    }
 };
